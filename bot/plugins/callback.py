9c9dd818ecaf47334d1130be9b3448b354722e77
ottguhan <86597680+ottguhan@users.noreply.github.com>
2021-07-18 21:53:15 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1639,10 +1639,8 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Filter-Bot-V2')
-        ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('Channel', url='https://t.me/Ottrockers2k'),
+            InlineKeyboardButton('Group ', url ='https://t.me/Tentkottagroup')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
